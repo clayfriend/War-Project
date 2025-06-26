3642f54edc05250ceacd47d1907b0bf1722d64f9
clayfriend <muminov0101@mail.ru>
2025-06-26 17:59:18 +0500

    Determine the winner

--- index.js
+++ index.js
@@ -22,9 +22,48 @@ document.getElementById("draw-deck").addEventListener('click', ()=>{
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res=>res.json())
     .then(data=>{
-        document.getElementById("cards").innerHTML=`
-            <img src=${data.cards[0].image}>
-            <img src=${data.cards[1].image}>
+        const computerCard = data.cards[0]
+        const playerCard=data.cards[1]
+
+        const cardSlot=document.getElementById("cards").children
+         cardSlot[0].innerHTML=`
+            <img src=${data.cards[0].image} class="card">      
         `
+        cardSlot[1].innerHTML=`     
+            <img src=${data.cards[1].image} class="card">      
+        `
+        const result = determineWinner(computerCard,playerCard)
+        console.log(result)
+
     })
 })
+
+function determineWinner(card1,card2){
+    const valueMap = {
+        "ACE": 14,
+        "KING": 13,
+        "QUEEN": 12,
+        "JACK": 11,
+        "10": 10,
+        "9": 9,
+        "8": 8,
+        "7": 7,
+        "6": 6,
+        "5": 5,
+        "4": 4,
+        "3": 3,
+        "2": 2
+    }
+
+    const card1Value = valueMap[card1.value]
+    const card2Value = valueMap[card2.value]
+
+    if(card1Value>card2Value){
+        return "Computer Won!"
+    } else if(card2Value>card1Value){
+        return "You Won!"
+    } else{
+        return "It's a tie!"
+    }
+
+}
