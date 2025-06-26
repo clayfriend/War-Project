5c374559718c8188fd30e2a1aa6a299147e2e21e
clayfriend <muminov0101@mail.ru>
2025-06-26 15:54:41 +0500

    fetchjing data

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let x = 10
+let deckId;
 
-function Sum(){
-    console.log(x)
-    let x=11
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
 }
 
-Sum()
+
+document.getElementById("new-deck").addEventListener('click',handleClick);
+
+
+
+document.getElementById("draw-deck").addEventListener('click', ()=>{
+    if(!deckId){
+        alert("Please First click the New Deck")
+        return
+    }
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res=>res.json())
+    .then(data=>{
+        document.getElementById("cards").innerHTML=`
+            <img src=${data.cards[0].image}>
+            <img src=${data.cards[1].image}>
+        `
+    })
+})
